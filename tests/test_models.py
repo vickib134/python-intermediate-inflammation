b15e22ee5accbe632940d5465d2df6019aea021b
Vicki Balfour <vb28@st-andrews.ac.uk>
2025-05-29 14:22:07 +0100

    Add tests for min and max functions

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,13 +2,16 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max, daily_min
+# An alternative way to do this rather than listing all functions would be 
+# to just do 'import inflammation.models'
+# To then call these you would need to do r.g. 'inflammation.models.daily_mean'
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
     
-
     test_input = np.array([[0, 0],
                            [0, 0],
                            [0, 0]])
@@ -29,3 +32,34 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_min_integers():
+    """Test that min function works for an array of positive integers."""
+
+    test_input = np.array([[2, 2],
+                           [6, 4],
+                           [6, 8]])
+    test_result = np.array([2, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_max_zeros():
+    """Test that mean function works for an array of zeros."""
+    
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'],['General', 'Kenobi']])
